c0c361b79654b6f15fdcc8fc091e0ec436fa0e69
shannietron <git@shan.mp>
2017-07-24 16:23:07 -0400

    Display score to hint on hand controller

--- Assets/scoreManager.cs
+++ Assets/scoreManager.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scoreManager : MonoBehaviour {
 
     public static int score;
-	// Use this for initialization
-	void Start () {
+    Text text;
+
+    void Awake () {
+        text = GetComponentInChildren<Text>();
+        Debug.Log(text.text);
+        score = 0;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log(score);
+        text.text = "Score "+ score;
+        
 	}
 }
+    
