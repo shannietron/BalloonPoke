1341d73e7a8ea57e02464c18af2a1439bfe5b2fb
shannietron <git@shan.mp>
2018-08-27 20:32:37 -0400

    Highlight selected balloon on enter and revert on exit

--- Assets/timeCollision.cs
+++ Assets/timeCollision.cs
@@ -6,11 +6,15 @@ public class timeCollision : MonoBehaviour {
 
     public static float delayDestroy = 0.1f; //time in seconds before balloon gets destroyed
     private float duration = 0;
+    private Renderer rend;
+    private Shader originalShader;
+    private Shader highlightShader;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        highlightShader = Shader.Find("Flare");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,14 +25,15 @@ public class timeCollision : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        GetComponent<Renderer>().material.color = Color.green;
+        originalShader = GetComponent<Renderer>().material.shader;
+        GetComponent<Renderer>().material.shader = highlightShader;
         //Debug.Log(gameObject.name + "was triggered by!" + other.gameObject.name);
         duration = 0;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        GetComponent<Renderer>().material.color = Color.red;
+        GetComponent<Renderer>().material.shader = originalShader;
         //Debug.Log(gameObject.name + "was triggered by!" + other.gameObject.name);
         duration = 0;
     }
